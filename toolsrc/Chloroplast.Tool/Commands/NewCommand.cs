badf472082d491e84356c8d22d7973c321db895d
Wildling Dev <wildlingdev@outlook.com>
2022-02-07 21:56:22 -0500

    WIP: functionally complete ... testing next

--- toolsrc/Chloroplast.Tool/Commands/NewCommand.cs
+++ toolsrc/Chloroplast.Tool/Commands/NewCommand.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using System.Threading;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Chloroplast.Core;
 using Chloroplast.Core.Extensions;
-using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.CommandLine;
 
 namespace Chloroplast.Tool.Commands
@@ -31,33 +29,23 @@ namespace Chloroplast.Tool.Commands
             var files = Directory.GetFiles (from);
             foreach (string file in Directory.EnumerateFiles (from, "*.*", SearchOption.AllDirectories))
             {
-                using (FileStream sourceStream = File.Open (file, FileMode.Open))
+                FileStream sourceStream = File.Open (file, FileMode.Open);
                 {
+                    string relativePath = file.Replace (from, string.Empty);
+                    if (relativePath.StartsWith(Path.DirectorySeparatorChar))
+                    {
+                        relativePath = relativePath.Substring (1, relativePath.Length - 1);
+                    }
+
                     yield return new TemplateFileData
                     {
-                        RelativeFilePath = file,
+                        RelativeFilePath = relativePath,
                         Stream = sourceStream
                     };
                 }
             }
         }
 
-    }
-
-    public class NewTemplateUrlFetcher : INewTemplateFetcher
-    {
-        public IEnumerable<TemplateFileData> GetTemplateFiles (string fromUrl)
-        {
-            using (HttpClient client = new HttpClient ())
-            {
-                using (Stream stream = client.GetStreamAsync (fromUrl).Result)
-                {
-                    // parse result stream to get list of files
-                    // then iterate and return those streams
-                    throw new NotImplementedException ();
-                }
-            }
-        }
     }
 
     public class NewCommand : ICliCommand
@@ -100,37 +88,32 @@ namespace Chloroplast.Tool.Commands
 
             string to = args.Skip (1).First ();
 
-            // TODO: make folder in working directory with `to` as name, use that path
-
-
-            INewTemplateFetcher fetcher;
-            // if from is a url set fetcher to NewTemplateUrlFetcher
-            //if (from.IsUrl ())
-            //{
-            //    fetcher = new NewTemplateUrlFetcher ();
-            //}
-            //else
-            {
-                fetcher = new NewTemplateDiskFetcher ();
-            }
+            INewTemplateFetcher fetcher = new NewTemplateDiskFetcher ();
+            
 
             List<Task> copyTasks = new List<Task> ();
-
-            string w = Path.Combine ("some", "relative", "/path");
 
             // get the template files, and copy them to the destination
             var templateSourceFiles = fetcher.GetTemplateFiles (desiredTemplatePath);
             foreach (TemplateFileData file in templateSourceFiles)
             {
-                string fileName = Path.GetFileName (file.RelativeFilePath);
-                string destination = Path.Combine (to, fileName);
-                // fileName needs to be relative path from "root"
+                string destination = Path.Combine (to, file.RelativeFilePath);
+
+                Console.WriteLine ($"Creating file: {destination}");
+
                 destination.EnsureFileDirectory ();
 
-                using (FileStream destinationStream = File.Create (destination))
-                {
-                    copyTasks.Add( file.Stream.CopyToAsync (destinationStream));
-                }
+                FileStream destinationStream = File.Create (destination);
+
+                copyTasks.Add( file.Stream
+                    .CopyToAsync (destinationStream)
+                    .ContinueWith(t =>
+                    {
+                        t.Wait ();
+                        file.Stream.Dispose ();
+                        destinationStream.Dispose ();
+                    }));
+                
             }
 
             return copyTasks;
@@ -150,144 +133,3 @@ namespace Chloroplast.Tool.Commands
         }
     }
 }
-
-
-
-/*
-public class NewCommand : ICliCommand
-{
-
-    private IConfigurationRoot config;
-
-    public NewCommand ()
-    {
-    }
-
-    public string Name => "Init";
-
-    public async Task<IEnumerable<Task>> RunAsync (IConfigurationRoot config)
-    {
-        this.config = config;
-        string rootDir = AskValue ("root");
-        if (string.IsNullOrWhiteSpace (rootDir))
-            rootDir = Directory.GetCurrentDirectory ();
-
-        rootDir.EnsureDirectory ();
-
-        // make the config file
-        string title = AskValue ("title");
-        string description = AskValue ("description");
-        string siteConfigPath = rootDir.CombinePath ("SiteConfig.yml");
-
-        string configContent = string.Format (InitConfigFile, title, description);
-        await WriteFile (siteConfigPath, configContent);
-
-        // make templates
-        string templatePath = rootDir.CombinePath ("Templates").EnsureDirectory ();
-        await WriteFile (templatePath.CombinePath ("Default.cshtml"), InitDefaultTemplateFile);
-        await WriteFile (templatePath.CombinePath ("SiteFrame.cshtml"), InitFrameFile);
-
-        // make default area
-        string areaPath = rootDir.CombinePath ("Docs").EnsureDirectory ();
-        await WriteFile (areaPath.CombinePath ("index.md"), InitDefaultIndex);
-
-        return new[] { Task.Delay (1) };
-    }
-
-    public async Task WriteFile (string path, string content)
-    {
-        Console.WriteLine ($"writing {path}");
-        await File.WriteAllTextAsync (path, content);
-    }
-
-    public string AskValue (string key)
-    {
-        string value = this.config[key];
-        if (string.IsNullOrWhiteSpace (value))
-        {
-            Console.WriteLine ($"{key}: ");
-            value = Console.ReadLine ();
-        }
-        return value;
-    }
-
-    private readonly string InitConfigFile = @"---
-# Site configuration file sample for chloroplast
-
-# site basics
-title: {0}
-description: >-
-{1}
-
-# razor templates
-templates_folder: Templates
-
-# main site folder
-areas:
-- source_folder: /Docs
-output_folder: /
-";
-
-
-    private readonly string InitDefaultTemplateFile = @"
-<h1>Default!</h1>
-
-<h2>@Model.GetMeta(""title"")</h2>
-@if (@Model.HasMeta(""subtitle""))
-{
-<h3>@Model.GetMeta(""subtitle"")</h3>
-}
-<div>@Raw(Model.Body)</div>";
-
-    private readonly string InitFrameFile = @"@using System.Linq;
-@using System.Collections.Generic;
-@{
-string RenderNodes(IEnumerable<Chloroplast.Core.ContentNode> nodes)
-{
-<ol>
-    @foreach(var item in nodes)
-    {
-        <li>
-            <a href=""/@item.Slug"">@item.Title</a>
-            @if (item.Children.Any())
-            {
-                @Raw(RenderNodes(item.Children))
-            }
-        </li>
-    }
-</ol>
-return """";
-}
-}
-<!DOCTYPE html>
-<html lang=""en"">
-<head>
-<meta charset=""utf-8"" />
-<meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
-<title>@Model.GetMeta(""Title"")</title>
-<link href=""/assets/main.css"" rel=""stylesheet"" />
-</head>
-<body>
-<h1>Wilderness Labs</h1>
-<div style=""float:left;width:50%"">
-@Raw(RenderNodes(Model.Tree))
-</div>
-<div class=""maincontent"">
-@Raw(Model.Body)
-</div>
-</body>
-</html>
-";
-    private readonly string InitDefaultIndex = @"---
-template: Home
-title: Home
-subtitle: Documentation site
----
-
-# Welcome!
-
-To the docs. yay!
-";
-}
-}
-}*/
